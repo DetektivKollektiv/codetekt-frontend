76be514540aeab531fbc79c5dd705c4aa3a5b795
gormlabenz <gorm@labenz.io>
2026-01-05 14:43:58 +0100

    feat: update metadata title and description for Codetekt platform

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,9 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
-  title: 'Next.js and Supabase Starter Kit',
-  description: 'The fastest way to build apps with Next.js and Supabase',
+  title: 'Codetekt - Platform',
+  description:
+    'Wir sind codetekt: Durch unseren Trust-Checking-Ansatz ermöglichen wir es allen, die Vertrauenswürdigkeit von Informationen einzuschätzen.',
 };
 
 export default async function RootLayout({
