4687918236967736974ebd77759e80a8939f697b
gormlabenz <gorm@labenz.io>
2026-01-15 14:00:55 +0100

    feat: integrate dialog for case details in CaseCard component

--- components/review/case-card.tsx
+++ components/review/case-card.tsx
@@ -1,3 +1,10 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
 import { Case } from '@/lib/queries/getCase';
 import { getLocalDate } from '@/lib/utils';
 import Image from 'next/image';
@@ -18,35 +25,53 @@ interface CaseCardProps {
 
 const CaseCard: FC<CaseCardProps> = ({ case: caseData }) => {
   return (
-    <Card className="overflow-hidden">
-      {caseData.open_graph_data?.og_image ? (
-        <Image
-          height={168}
-          width={300}
-          src={caseData.open_graph_data.og_image}
-          alt={caseData.open_graph_data.og_image_alt || 'Case image'}
-        />
-      ) : (
-        <ImagePlaceholder
-          width={300}
-          height={168}
-          seed={caseData.case_number}
-        />
-      )}
-      <CardHeader>
-        <CardTitle>Fall {caseData.case_number}</CardTitle>
-        <CardDescription>
-          Eingereicht am: {getLocalDate(caseData.submitted_at)}
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        {caseData.content_type === 'url' ? (
-          <Link href={caseData.content}>{caseData.content}</Link>
-        ) : (
-          <p className="line-clamp-3">{caseData.content}</p>
-        )}
-      </CardContent>
-    </Card>
+    <>
+      <Dialog>
+        <DialogTrigger disabled={caseData.content_type !== 'text'}>
+          <Card
+            className={`overflow-hidden text-left ${
+              caseData.content_type === 'text' &&
+              'cursor-pointer hover:shadow-lg transition-shadow '
+            }`}
+          >
+            {caseData.open_graph_data?.og_image ? (
+              <Image
+                height={168}
+                width={300}
+                src={caseData.open_graph_data.og_image}
+                alt={caseData.open_graph_data.og_image_alt || 'Case image'}
+              />
+            ) : (
+              <ImagePlaceholder
+                width={300}
+                height={168}
+                seed={caseData.case_number}
+              />
+            )}
+            <CardHeader>
+              <CardTitle>Fall {caseData.case_number}</CardTitle>
+              <CardDescription>
+                Eingereicht am: {getLocalDate(caseData.submitted_at)}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              {caseData.content_type === 'url' ? (
+                <Link href={caseData.content}>{caseData.content}</Link>
+              ) : (
+                <p className="line-clamp-3">{caseData.content}</p>
+              )}
+            </CardContent>
+          </Card>
+        </DialogTrigger>
+
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+          </DialogHeader>
+          <p>{caseData.content}</p>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
