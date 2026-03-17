185038e9f42f6625c6591222ace28720831ddc71
gormlabenz <gorm@labenz.io>
2026-03-17 14:20:08 +0100

    feat: remove environment variable check from updateSession and utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,11 +7,6 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// This check can be removed, it is just for tutorial purposes
-export const hasEnvVars =
-  process.env.NEXT_PUBLIC_SUPABASE_URL &&
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
-
 export function getDisplayName(
   profile: Tables<'profiles'> | null,
   user: User | null,
