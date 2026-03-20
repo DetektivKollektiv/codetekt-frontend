95f8893a042e3d7c731bcfc8b5a4e07cf32146ec
gormlabenz <gorm@labenz.io>
2026-03-20 16:34:40 +0100

    feat: update warning message in ReviewersAvatarGroup for clarity

--- components/archive-detail/reviewers-avatar-group.tsx
+++ components/archive-detail/reviewers-avatar-group.tsx
@@ -115,7 +115,7 @@ export function ReviewersAvatarGroup({
             arrowClassName="bg-neutral-100 fill-neutral-100"
           >
             <p className="text-center">
-              Es haben weniger als <br /> 5 Reviewer den Fall bewertet.
+              Es haben weniger als <br /> 5 Detektive den Fall bewertet.
             </p>
           </TooltipContent>
         </Tooltip>
