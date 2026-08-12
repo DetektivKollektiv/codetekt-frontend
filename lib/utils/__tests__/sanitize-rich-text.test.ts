52ef13125d5ea5054d0db4aa5c5466fb027e988f
gormlabenz <gorm@labenz.io>
2026-08-12 16:48:54 +0200

    fix: correct internal link href in sanitizeRichText tests

--- lib/utils/__tests__/sanitize-rich-text.test.ts
+++ lib/utils/__tests__/sanitize-rich-text.test.ts
@@ -31,10 +31,10 @@ describe('sanitizeRichText', () => {
   it('keeps internal links', () => {
     expect(
       sanitizeRichText(
-        'Mehr Infos gibt’s <a href="/#challenge-information">hier</a>.',
+        'Mehr Infos gibt’s <a href="#challenge-information">hier</a>.',
       ),
     ).toBe(
-      'Mehr Infos gibt’s <a href="/#challenge-information">hier</a>.',
+      'Mehr Infos gibt’s <a href="#challenge-information">hier</a>.',
     );
   });
 
